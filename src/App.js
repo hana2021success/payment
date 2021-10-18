019396eaee3d0ae73d4b5eed3526a0467d00499b
JizongL <2015rpro@gmail.com>
2021-10-17 23:14:37 -0400

    finished core feature

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [amount, setAmount] = useState(0);
-  const [isAmountNotValid, setIsAmountNotValid] = useState(false);
+  const [installment, setInstallment] = useState(0);
+
+  // useEffect(() => {
+  //   createPaymentPlan(amount);
+  // }, [installment]);
   /**
    * This function calculates the installments for a purchase plan.
    * Assume there are always four installments.
@@ -13,25 +17,43 @@ function App() {
    * dollar amount and clicks submit.
    * @param {number} amount the amount the customer intends to purchase
    */
-  const createPaymentPlan = (amount) => {
-    const installment = amount / 4;
-    console.log("Creating payment plan with amount: $", installment);
+
+  const dataObject = [
+    { id: 1, order: "First", date: "2020" },
+    {
+      id: 2,
+      order: "Second",
+      date: "2020",
+    },
+    { id: 3, order: "Third", date: "2020" },
+    { id: 4, order: "Forth", date: "2020" },
+  ];
+
+  const CreatePaymentPlan = ({ amount }) => {
+    return dataObject.map((item, index) => (
+      <li key={index}>
+        <div>{item.date}</div>
+        <div>{item.order} payment</div>
+        <div>$ {amount}</div>
+      </li>
+    ));
   };
 
   const handleOnChange = (event) => {
     const amount = event.target.value;
     setAmount(amount);
+    const obj = { paymentDate: "", paymentOrder: "", amount: "" };
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    createPaymentPlan(amount);
+    let installmentAmount = (amount / 4).toFixed(2);
+    setInstallment(installmentAmount);
   };
-
+  console.log({ installment });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {isAmountNotValid && <span>please enter numerical value</span>}
         <form onSubmit={handleSubmit}>
           <label>How much do you plan to spend?</label>
           <input
@@ -40,8 +62,9 @@ function App() {
             id="amount"
             placeholder="$ "
           />
-          <input disabled={isAmountNotValid} type="submit" value="Submit" />
+          <input type="submit" value="Submit" />
         </form>
+        {installment > 0 && <CreatePaymentPlan amount={installment} />}
       </header>
     </div>
   );
