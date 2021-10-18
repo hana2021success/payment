b759dd2aa91017371a43422a42c813069135820b
JizongL <2015rpro@gmail.com>
2021-10-18 15:12:18 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -3,46 +3,52 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 
 function App() {
+  const dataObject = [
+    { id: 1, order: "First", date: "2021 July 1st" },
+    { id: 2, order: "Second", date: "2021 July 14th" },
+    { id: 3, order: "Third", date: "2021 July 28th" },
+    { id: 4, order: "Forth", date: "2021 August 12th" },
+  ];
   const [amount, setAmount] = useState(0);
-  const [isAmountNotValid, setIsAmountNotValid] = useState(false);
-  /**
-   * This function calculates the installments for a purchase plan.
-   * Assume there are always four installments.
-   * The function need not return anything but should be used to display
-   * the four installments back to the user after the user inputs a valid
-   * dollar amount and clicks submit.
-   * @param {number} amount the amount the customer intends to purchase
-   */
-  const createPaymentPlan = (amount) => {
-    const installament = amount / 4;
-    console.log("Creating payment plan with amount: $", installament);
+  const [installment, setInstallment] = useState(null);
+  const CreatePaymentPlan = ({ amount }) => {
+    const result = dataObject.map((item, index) => {
+      console.log(index);
+      return (
+        <li className="item-container" key={index}>
+          <div className="item">
+            <div className="item-left">
+              <div>{item.date}</div>
+              <div>{item.order} Payment</div>
+            </div>
+            <div className="item-right">${amount}</div>
+          </div>
+        </li>
+      );
+    });
+    console.log("23", result);
+    return <ul>{result}</ul>;
   };
 
-  const handleOnChange = (event) => {
-    const amount = event.target.value;
-    setAmount(amount);
+  const handleUpdateAmount = (e) => {
+    console.log(e.target.value);
+    setAmount(e.target.value);
   };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    createPaymentPlan(amount);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const installment = amount / 4;
+    setInstallment(installment);
   };
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {isAmountNotValid && <span>please enter numerical value</span>}
         <form onSubmit={handleSubmit}>
           <label>How much do you plan to spend?</label>
-          <input
-            onChange={handleOnChange}
-            type="float"
-            id="amount"
-            placeholder="$"
-          />
-          <input disabled={isAmountNotValid} type="submit" value="submit" />
+          <input onChange={handleUpdateAmount} type="float" id="amount" />
+          <input type="submit" value="submit" />
         </form>
+        <CreatePaymentPlan amount={installment} />
       </header>
     </div>
   );
